6f2d48c9b8eba796c06195ed726458da63eb3824
Brian Engelhardt <briatt22@gmail.com>
2017-07-20 21:15:30 -0500

    So I have a place for the app to go now.
    Webpack and Babel set up.

---
+++ main.js
@@ -0,0 +1,5 @@
+/**
+ * Main entry point for application! YAY!
+ */
+
+console.log('something happened here!');
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/*
+ ./webpack.config.js
+ */
+const path = require('path');
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'main.bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+		]
+	}
+}
