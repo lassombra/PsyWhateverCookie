b1d597cdb7bfe70f0eef4a7fea7d614aa8d44e1f
Brian Engelhardt <briatt22@gmail.com>
2017-07-20 23:45:33 -0500

    Adding redux

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import store from './redux';
+import { Provider } from 'react-redux'
 
 export default class App extends React.Component {
 	render() {
-		return <table>
-			<tr>
-				<td colSpan="4">Score: 0</td>
-			</tr>
-			<tr>
-				<td>Cookie will be here</td>
-				<td colSpan="2">Upgrades will be here</td>
-				<td>Upgrade purchase will be here</td>
-			</tr>
-		</table>
+		return <Provider store={store}>
+			<table>
+				<tr>
+					<td colSpan="4">Score: 0</td>
+				</tr>
+				<tr>
+					<td>Cookie will be here</td>
+					<td colSpan="2">Upgrades will be here</td>
+					<td>Upgrade purchase will be here</td>
+				</tr>
+			</table>
+		</Provider>
 	}
 }
